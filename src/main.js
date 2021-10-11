8e063e3872ef99bf02511063fa93643d9dfb21d2
krai54rus <krai54rus@gmail.com>
2021-10-11 13:38:17 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import router from './router'
+import store from './store'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+Vue.use(VueRouter)
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from '../components/Main.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path:'/',
+    name:'',
+    component: Main,
+  },
+  {
+    path: '*',
+    component: () => Main,
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router
